080e7eb7c4b4f10f223d06b0a0c247cdbdd26fa7
lnwu <wind2729@gmail.com>
2019-06-08 01:15:07 +0800

    Li: setup express

---
+++ src/handler.ts
@@ -0,0 +1,9 @@
+import { APIGatewayEvent, Handler, Context } from "aws-lambda"
+import * as awsServerlessExpress from "aws-serverless-express"
+import { app } from "./app"
+
+const server = awsServerlessExpress.createServer(app)
+export const handler: Handler = async (
+  event: APIGatewayEvent,
+  context: Context
+) => awsServerlessExpress.proxy(server, event, context)
