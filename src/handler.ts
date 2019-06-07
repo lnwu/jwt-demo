ccd1dd3aa99a6a56c9c52c67f8a42a09a12452bb
lnwu <wind2729@gmail.com>
2019-06-08 01:27:23 +0800

    Li: remove async handler not work issue

--- src/handler.ts
+++ src/handler.ts
@@ -6,4 +6,4 @@ const server = awsServerlessExpress.createServer(app)
 export const handler: Handler = async (
   event: APIGatewayEvent,
   context: Context
-) => awsServerlessExpress.proxy(server, event, context)
+) => awsServerlessExpress.proxy(server, event, context, "PROMISE").promise
