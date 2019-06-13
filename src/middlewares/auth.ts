dbc04e30e202625c3e8a9a8c22be4eb502aeef49
lnwu <wind2729@gmail.com>
2019-06-13 20:40:39 +0800

    Li: use signed user id

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 import { Request, Response, NextFunction } from "express"
+import { readFileSync } from "fs"
+import { createVerify } from "crypto"
+import { ALG } from "../routes/user"
 
 declare global {
   namespace Express {
@@ -9,11 +12,21 @@ declare global {
   }
 }
 
+const publicKey = readFileSync("/Users/lnwu/Dev/test/public.pem", "utf8")
+
 export const auth = () => (req: Request, res: Response, next: NextFunction) => {
-  const authCookie = req.cookies.auth
+  const authCookie = req.cookies.auth as string
   if (authCookie) {
-    req.userId = authCookie
-    next()
+    const [userId, signedUserId] = authCookie.split(".")
+    const verifyResult = createVerify(ALG)
+      .update(userId)
+      .verify(publicKey, signedUserId, "hex")
+    if (verifyResult) {
+      req.userId = userId
+      next()
+    } else {
+      res.status(401).send({ message: "UNAUTHORIZED" })
+    }
   } else {
     res.status(401).send({ message: "UNAUTHORIZED" })
   }
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,6 +4,11 @@ import {
   getUserIdByEmailAndPassword,
   updateUserById
 } from "../db/user.db"
+import { readFileSync } from "fs"
+import { createSign } from "crypto"
+
+const privateKey = readFileSync("/Users/lnwu/Dev/test/private.pem", "utf8")
+export const ALG = "RSA-SHA256"
 
 export const login = async (req: Request, res: Response) => {
   const { email, password } = req.body
@@ -11,9 +16,15 @@ export const login = async (req: Request, res: Response) => {
     res.status(401).send({ message: "Email/password is required!" })
   }
 
-  const userId = getUserIdByEmailAndPassword(email, password)
-  if (await userId) {
-    res.cookie("auth", await userId, { httpOnly: true })
+  const userId = await getUserIdByEmailAndPassword(email, password)
+  if (userId) {
+    const sign = createSign(ALG)
+    sign.update(userId)
+    const signedUserId = sign.sign(privateKey, "hex")
+    res.cookie("auth", `${userId}.${signedUserId}`, {
+      httpOnly: true,
+      maxAge: 1000 * 60
+    })
     res.send({ message: "Login succeed!" })
   } else {
     res.status(401).send({ message: "Email/password is wrong!" })
