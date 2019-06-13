738065f540421371688f2fb398e56dfe07a4dbb6
lnwu <wind2729@gmail.com>
2019-06-13 20:29:27 +0800

    Li: set auth token using jwt

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,9 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 import { Request, Response, NextFunction } from "express"
+import { createVerify } from "crypto"
+import { readFileSync } from "fs"
+import base64url from "base64url"
+import { ALG } from "../routes/user"
 
 declare global {
   namespace Express {
@@ -9,11 +13,24 @@ declare global {
   }
 }
 
+const publicKey = readFileSync("/Users/lnwu/Dev/test/public.pem", "utf8")
+
 export const auth = () => (req: Request, res: Response, next: NextFunction) => {
-  const authCookie = req.cookies.auth
+  const authCookie = req.cookies.auth as string
   if (authCookie) {
-    req.userId = authCookie
-    next()
+    const verify = createVerify(ALG)
+    const [header, payload, signature] = authCookie.split(".")
+
+    verify.update(`${header}.${payload}`)
+
+    const verifyResult = verify.verify(publicKey, signature, "hex")
+    if (verifyResult) {
+      const payloadObject = JSON.parse(base64url.decode(payload))
+      req.userId = payloadObject.sub
+      next()
+    } else {
+      res.status(401).send({ message: "UNAUTHORIZED" })
+    }
   } else {
     res.status(401).send({ message: "UNAUTHORIZED" })
   }
